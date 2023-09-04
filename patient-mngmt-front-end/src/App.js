a2a5259d44aa99e75207f7a3e53dd4c180b17d03
KOMAL <66866606+Komal-Mahajan-3119143664812483@users.noreply.github.com>
2023-09-04 17:05:21 +0530

    modified:   patient-mngmt-front-end/src/App.js 	deleted:    patient-mngmt-front-end/src/Components/Login.js 	deleted:    patient-mngmt-front-end/src/Components/Registration.js

--- patient-mngmt-front-end/src/App.js
+++ patient-mngmt-front-end/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './Components./Login';
-import Registration from './Components./Registration';
+import Login from './Components/Login.js';
+import Registration from './Components/Registration.js';
 
 function App() {
   return (
